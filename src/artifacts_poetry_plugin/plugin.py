7bdf3f27ff1b2ab9136a6afcd08429eee9fa720c
Robert Delametter <850129@bah.com>
2024-02-02 14:41:16 -0600

    Made more feedback changes

--- src/artifacts_poetry_plugin/plugin.py
+++ src/artifacts_poetry_plugin/plugin.py
@@ -33,132 +33,133 @@ class RepoData:
         return self.password
 
 
-def get_repository_data(config, repo_name, io):
-    """Gets the URL, username, and password from the poetry configuration for the given repo name"""
-    err_msg = f"username and password are required. Please verify that your poetry config for this repository is correct for repository {repo_name}"
-    url = config.get(f"repositories.{repo_name}.url")
-    if url is None:
-        raise RuntimeError(f"Repository {repo_name} is not defined")
-    authenticator = Authenticator(config, io)
-    auth = authenticator.get_http_auth(repo_name)
-    username = None
-    password = None
-    if auth:
-        username = auth.username
-        password = auth.password
-    else:
-        raise RuntimeError(err_msg)
-    if username is None or password is None:
-        raise RuntimeError(err_msg)
-    return RepoData(url, username, password)
-
-
-def get_packages(poetry) -> List[Package]:
-    locker = poetry.locker
-    repository = locker.locked_repository()
-    return repository.packages
-
-
-def get_cached_wheel_files(cache_dir):
-    """Gets all wheel file locations for the given cache directory"""
-    wheel_files = {}
-    for root, dirs, files in os.walk(cache_dir):
-        for file in files:
-            if file.endswith(".whl"):
-                wheel_files[file] = Path(os.path.join(root, file))
-    return wheel_files
-
-
-def get_deploy_non_deploy_packages(packages: List[Package], wheel_files):
-    """Adds the wheel file locations for each package's files"""
-    non_deployable_packages = []
-    deployable_packages = []
-    for package in packages:
-        file_found = False
-        for file in package.files:
-            if file["file"] in wheel_files:
-                file_found = True
-                file["url"] = wheel_files[file["file"]]
-        if file_found:
-            deployable_packages.append(package)
-        else:
-            non_deployable_packages.append(package)
-    return deployable_packages, non_deployable_packages
-
-
-def upload_packages(poetry, packages: List[Package], url, username, password, io):
-    uploader = Uploader(poetry=poetry, io=io)
-    uploader.auth(username=username, password=password)
-    session = uploader.make_session()
-    for package in packages:
-        uploader._package = package
-        for file in package.files:
-            if "url" in file:
-                uploader._upload_file(
-                    session,
-                    url,
-                    file=file["url"],
-                )
-
-
-def get_project_package(poetry):
-    """Adds the generated package to the list of packages with its associated wheel file"""
-    project_package = poetry.package
-    for root, dirs, files in os.walk("dist/"):
-        print(files)
-        for file in files:
-            if file.endswith(".whl"):
-                file_data = {"file": file, "url": Path(os.path.join(root, file))}
-                project_package.files.append(file_data)
-    return project_package
-
-
-def get_all_poetry(path):
-    poetry_files = []
-    for root, dirs, files in os.walk(Path(path)):
-        for file in files:
-            if file == "pyproject.toml":
-                poetry_files.append(Factory().create_poetry(Path(root)))
-    return poetry_files
-
-
-def get_deploy_packages(root_dir, cache_dir):
-    """Gets all deployable packages from the given cache directory. A package is deployable if it has any wheel files associated with it"""
-    total_deployable_packages = []
-    total_non_deployable_packages = []
-    poetry_files = get_all_poetry(root_dir)
-    for poetry in poetry_files:
-        packages = get_packages(poetry)
-        wheel_files = get_cached_wheel_files(cache_dir)
-        deployable_packages, non_deployable_packages = get_deploy_non_deploy_packages(
-            packages=packages, wheel_files=wheel_files
-        )
-        total_deployable_packages.extend(deployable_packages)
-        total_non_deployable_packages.extend(non_deployable_packages)
-        total_deployable_packages.append(get_project_package(poetry))
-    return total_deployable_packages, total_non_deployable_packages
-
-
 class ArtifactsDeploy(Command):
     name = ARTIFACTS_DEPLOY
 
     arguments = [
-        argument(REPOSITORY_NAME, "The name of the repository to deploy artifacts")
+        argument(
+            REPOSITORY_NAME,
+            "The name of the repository to which artifacts should be deployed",
+        )
     ]
 
     help = "The Artifacts Deploy command is used to deploy all of a Python project's dependencies to a repository"
+
+    def get_repository_data(self, config, repo_name, io):
+        """Gets the URL, username, and password from the poetry configuration for the given repo name"""
+        err_msg = f"username and password are required. Please verify that your poetry config for this repository is correct for repository {repo_name}"
+        url = config.get(f"repositories.{repo_name}.url")
+        if url is None:
+            raise RuntimeError(f"Repository {repo_name} is not defined")
+        authenticator = Authenticator(config, io)
+        auth = authenticator.get_http_auth(repo_name)
+        username = None
+        password = None
+        if auth:
+            username = auth.username
+            password = auth.password
+        else:
+            raise RuntimeError(err_msg)
+        if username is None or password is None:
+            raise RuntimeError(err_msg)
+        return RepoData(url, username, password)
+
+    def get_poetry_packages(self, poetry) -> List[Package]:
+        """Gets packages from the given poetry.lock file"""
+        locker = poetry.locker
+        repository = locker.locked_repository()
+        return repository.packages
+
+    def get_cached_wheel_files(self, cache_dir):
+        """Gets all wheel file locations for the given cache directory"""
+        wheel_files = {}
+        for root, dirs, files in os.walk(cache_dir):
+            for file in files:
+                if file.endswith(".whl"):
+                    wheel_files[file] = Path(os.path.join(root, file))
+        return wheel_files
+
+    def get_deploy_non_deploy_packages(self, packages: List[Package], wheel_files):
+        """Adds the wheel file locations for each package's files"""
+        non_deployable_packages = []
+        deployable_packages = []
+        for package in packages:
+            file_found = False
+            for file in package.files:
+                if file["file"] in wheel_files:
+                    file_found = True
+                    file["url"] = wheel_files[file["file"]]
+            if file_found:
+                deployable_packages.append(package)
+            else:
+                non_deployable_packages.append(package)
+        return deployable_packages, non_deployable_packages
+
+    def upload_packages(
+        self, poetry, packages: List[Package], url, username, password, io
+    ):
+        uploader = Uploader(poetry=poetry, io=io)
+        uploader.auth(username=username, password=password)
+        session = uploader.make_session()
+        for package in packages:
+            uploader._package = package
+            for file in package.files:
+                if "url" in file:
+                    uploader._upload_file(
+                        session,
+                        url,
+                        file=file["url"],
+                    )
+
+    def get_project_package(self, poetry):
+        """Adds the generated package to the list of packages with its associated wheel file"""
+        project_package = poetry.package
+        for root, dirs, files in os.walk("dist/"):
+            print(files)
+            for file in files:
+                if file.endswith(".whl"):
+                    file_data = {"file": file, "url": Path(os.path.join(root, file))}
+                    project_package.files.append(file_data)
+        return project_package
+
+    def get_all_poetry(self, path):
+        poetry_files = []
+        for root, dirs, files in os.walk(Path(path)):
+            for file in files:
+                if file == "pyproject.toml":
+                    poetry_files.append(Factory().create_poetry(Path(root)))
+        return poetry_files
+
+    def create_packages(self, root_dir, cache_dir):
+        """Gets all deployable packages required by each downstream project from the given cache directory. A package is deployable if it has any wheel files associated with it"""
+        total_deployable_packages = []
+        total_non_deployable_packages = []
+        poetry_files = self.get_all_poetry(root_dir)
+        for poetry in poetry_files:
+            packages = self.get_poetry_packages(poetry)
+            wheel_files = self.get_cached_wheel_files(cache_dir)
+            (
+                deployable_packages,
+                non_deployable_packages,
+            ) = self.get_deploy_non_deploy_packages(
+                packages=packages, wheel_files=wheel_files
+            )
+            total_deployable_packages.extend(deployable_packages)
+            total_non_deployable_packages.extend(non_deployable_packages)
+            total_deployable_packages.append(self.get_project_package(poetry))
+        return total_deployable_packages, total_non_deployable_packages
 
     def handle(self) -> int:
         self.line("Artifacts Deploy")
         repo_name = self.argument(REPOSITORY_NAME)
         poetry = Factory().create_poetry(Path(os.getcwd()))
         config = poetry.config
-        packages, non_deployable_packages = get_deploy_packages(
+        packages, non_deployable_packages = self.create_packages(
             os.getcwd(),
             config.artifacts_cache_directory.absolute().as_posix(),
         )
-        repo_data = get_repository_data(config, repo_name, self.io)
-        upload_packages(
+        repo_data = self.get_repository_data(config, repo_name, self.io)
+        self.upload_packages(
             poetry,
             packages=packages,
             url=repo_data.url,
